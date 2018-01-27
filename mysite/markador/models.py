71dfe66d56e26cbcb78eca49767458d7a5feceaa
jay ess <mentoce@gmail.com>
2018-01-27 15:21:35 -0800

    Update Bookmark model: auto add date

--- mysite/markador/models.py
+++ mysite/markador/models.py
@@ -8,9 +8,14 @@ class Bookmark(models.Model)
     description = models.TextField()
     is_public = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True, 'date created')
-    date_updated = models.DateTimeField(auto_now_add=True, 'date updated')
+    date_updated = models.DateTimeField(null=True,auto_now_add=True, 'date updated')
     owner = models.ForeignKey(User, related_name='bookmarks')
     tags = models.ManytoManyField(Tag, blank=True)
+
+    class Meta:
+        verbose_name = 'bookmark'
+        verbose_name_plural = 'bookmarks'
+        ordering = ['-date_created']
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
