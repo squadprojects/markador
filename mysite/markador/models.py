e534fbfbe2b77638009cb73f6d71efeedaacc1e6
David Rojas <rojas32@gmail.com>
2018-01-27 15:08:50 -0800

    created bookmark model

--- mysite/markador/models.py
+++ mysite/markador/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Bookmark(models.Model)
+    url = models.URLField()
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    is_public = models.BooleanField()
+    date_created = models.DateTimeField('date created')
+    date_updated = models.DateTimeField('date updated')
+    owner = models.ForeignKey(User, related_name='bookmarks')
+    tags = models.ManytoManyField(Tag, blank=True)
