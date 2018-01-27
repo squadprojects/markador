f9334c4cae6b0353715a30de6fbf72e3b9654d6b
jay ess <mentoce@gmail.com>
2018-01-27 15:56:54 -0800

    In markador/admin.py: create new BookmarkAdmin class and register it and Tag

--- mysite/markador/admin.py
+++ mysite/markador/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Bookmark
 
-# Register your models here.
-admin.site.register(Bookmark)
+from .models import Bookmark, Tag
+
+
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('url', 'title', 'owner', 'is_public', 'date_updated')
+    list_editable = ('is_public',)
+    list_filter = ('is_public', 'owner__username')
+    search_fields = ['url', 'title', 'description']
+    readonely_fields = ('date_created', 'date_updated')
+
+
+admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(Tag)
