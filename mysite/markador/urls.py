8d86c28b9459dff2002996590ab5816863c30d8a
jay ess <mentoce@gmail.com>
2018-01-31 11:01:49 -0800

    Add missing 'P' in route

--- mysite/markador/urls.py
+++ mysite/markador/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^user/(?<username>[-\w]+)/$', views.bookmark_user, name='bookmark_user'),
+    url(r'^user/(?P<username>[-\w]+)/$', views.bookmark_user, name='bookmark_user'),
     url(r'^$', views.bookmark_list, name='bookmark_list'),
     url(r'^(?P<bookmark_id>[0-9]+)/$', views.detail, name='bookmark_details'),        
 ]
