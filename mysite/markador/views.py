41618355c364e7900e2f48b8805e985e402fb427
jay ess <mentoce@gmail.com>
2018-01-31 11:04:16 -0800

    Create dummy views for markador app

--- mysite/markador/views.py
+++ mysite/markador/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Bookmark
+
+def bookmark_user(request, username):
+    return HttpResponse("You're looking at the view for user %s" % username)
+
+def bookmark_list(request):
+    # bookmarks = Bookmark
+    return HttpResponse("You're looking at the view for bookmark list")
+    
+
 def detail(request, bookmark_id):
     return HttpResponse("You're looking at bookmark %s." % bookmark_id)
+
 
