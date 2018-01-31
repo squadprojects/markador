89bee43f8a950158aaa30a527641ec4edf9d9c37
David Rojas <rojas32@gmail.com>
2018-01-31 11:15:47 -0800

    created real bookmark list view

--- mysite/markador/views.py
+++ mysite/markador/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from .models import Bookmark
 
+
 def bookmark_user(request, username):
     return HttpResponse("You're looking at the view for user %s" % username)
 
+
 def bookmark_list(request):
-    # bookmarks = Bookmark
-    return HttpResponse("You're looking at the view for bookmark list")
-    
+    bookmarks = Bookmark.public.all()
+    context = {'bookmarks': bookmarks}
+    return render(request, 'marcador/bookmark_list.html', context)
+
 
 def detail(request, bookmark_id):
     return HttpResponse("You're looking at bookmark %s." % bookmark_id)
-
-
