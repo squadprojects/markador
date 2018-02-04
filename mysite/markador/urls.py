8338db849cbcf3f3b8b4d2df8d3c612a00905639
jay ess <mentoce@gmail.com>
2018-02-03 21:23:43 -0800

    Did a lot

--- mysite/markador/urls.py
+++ mysite/markador/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^user/(?P<username>[-\w]+)/$', views.bookmark_user, name='bookmark_user'),
     url(r'^$', views.bookmark_list, name='bookmark_list'),
-    url(r'^(?P<bookmark_id>[0-9]+)/$', views.detail, name='bookmark_details'),        
+    url(r'^(?P<bookmark_id>[0-9]+)/$', views.detail, name='bookmark_details'),
+    url(r'^create/$', views.bookmark_create, name='bookmark_create'),
+    url(r'^edit/(?P<pk>\d+)/$', views.bookmark_edit, name='bookmark_edit'),
 ]
 
--- mysite/markador/views.py
+++ mysite/markador/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 
+from .forms import BookmarkForm
 from .models import Bookmark
 
 
@@ -22,3 +25,35 @@ def bookmark_list(request):
 
 def detail(request, bookmark_id):
     return HttpResponse("You're looking at bookmark %s." % bookmark_id)
+
+
+@login_required
+def bookmark_create(request):
+    if request.method == 'POST':
+        form = BookmarkForm(data=request.POST)
+        if form.is_valid():
+            bookmark = form.save(commit=False)
+            bookmark.owner = request.user
+            bookmark.save()
+            form.save_m2m()
+            return redirect('bookmark_user', username=request.user.username)
+    else:
+        form = BookmarkForm()
+    context = {'form': form, 'create': True}
+    return render(request, 'markador/form.html', context)
+
+
+@login_required
+def bookmark_edit(request, pk):
+    bookmark = get_object_or_404(Bookmark, pk=pk)
+    if bookmark.owner != request.user and not request.user.is_superuser:
+        raise PermissionDeniesd
+    if request.method == 'POST':
+        form = BookmarkForm(instance=bookmark, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('bookmark_user', username=request.user.username)
+    else:
+        form = BookmarkForm(instance=bookmark)
+    context = {'form': form, 'create': False}
+    return render(request, 'markador/form.html', context)
