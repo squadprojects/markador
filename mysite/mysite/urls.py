395bf3b067f7871aa2c21493d07e49807937dd5d
David Rojas <rojas32@gmail.com>
2018-02-03 20:19:05 -0800

    added login logout routes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    url(r'^bookmarks/', include('markador.urls')), 
+    url(r'^bookmarks/', include('markador.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'},
+        name='mysite_login'),
+    url(r'^logout/$', auth_views.logout,
+        {'next_page': reverse_lazy('bookmark_list')},
+        name='mysite_logout'),
+
 ]
